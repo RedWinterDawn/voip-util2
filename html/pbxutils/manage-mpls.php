0a06b3aa94269eee7556c1a5b9c596aba7724341
jive-builder <it+jenkins@getjive.com>
2014-06-06 12:41:57 -0600

    added output after input

--- html/pbxutils/manage-mpls.php
+++ html/pbxutils/manage-mpls.php
@@ -66,13 +66,13 @@ if (preg_match('/[^0-9]/i', $customer))
 		echo "<hr>";
 		$action = null;
 }
-if (preg_match('/[^a-z]/i', $domain))
-{
-	    echo "<p class='red'> Invalid Input for Domain! <br/> Use letters only.</p>";
-		echo "<p>You gave the following: ".$domain."</p>";
-		echo "<hr>";
-		$action = null;
-}
+//if (preg_match('/[^a-z]/i', $domain))
+//{
+//	    echo "<p class='red'> Invalid Input for Domain! <br/> Use letters only.</p>";
+//		echo "<p>You gave the following: ".$domain."</p>";
+//		echo "<hr>";
+//		$action = null;
+//}
 //if (preg_match('/[^a-z_\-0-9]/i', $location))
 //{
 //	    echo "<p class='red'> Invalid Input for Location! <br/> Use letters, numbers, underscores, and hyphens only.</p>";
@@ -110,7 +110,7 @@ if ($action == $gobutton)
 		}
 		//If both status and location were set, "action" would have been set at least twice by now
 		//So we'll check to see if they're both set and update "action" if they are.
-		if (($order != "") && ($customer != "") && ($location != "") && ($carrier != "") && ($lec != "") && ($lan != "") && ($wan != ""))
+		if (($order != "") && ($customer != "") && ($location != "") && ($carrier != "") && ($lan != "") && ($wan != ""))
 		{
 			$action = "add";
 		}
@@ -137,13 +137,22 @@ switch ($action)
 {
 	case "add":
 		$rodb = pg_connect("host=rodb dbname=pbxs user=postgres ") or die('Could not connect to "pbxs" database: ' . pg_last_error());
-		$name = pg_fetch_row(pg_query($rodb, "SELECT * FROM resource_group WHERE domain='".$domain."'")) or die('Could not find domain: '.$domain. ' '.pg_last_error());
+		$name = pg_fetch_row(pg_query($rodb, "SELECT name FROM resource_group WHERE domain='".$domain."'")) or die('Could not find domain: '.$domain. ' '.pg_last_error());
 		pg_close($rodb);
 
 		$insert = "INSERT INTO mpls VALUES ('".$carrier."', ".$customer.", '".$domain."', DEFAULT, '".$lec."', '".$location."', '".$name[0]."', ".$order.", '".$wan."', '".$lan."');";
 		pg_query($dbconn, $insert) or die('Could not add to mpls! ' . pg_last_error());
 		 
 		// echo what was added need to do 
+		echo "Inserted the following into mpls:<br>";
+		echo "Domain: ".$domain."<br>";
+		echo "Order#: ".$order."<br>";
+		echo "Customer#: ".$customer."<br>";
+		echo "Location: ".$location."<br>";
+		echo "Carrier Circuit ID: ".$carrier."<br>";
+		echo "LEC Circuit ID: ".$lec."<br>";
+		echo "Public LAN IPs: ".$lan."<br>";
+		echo "Private WAN IPs: ".$wan."<br>";
 		echo "<hr>";
 		break;
 }
