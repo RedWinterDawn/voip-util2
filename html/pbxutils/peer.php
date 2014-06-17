2a324b98afa490c06da7eeb665d5a12ae00c82c7
jive-builder <it+jenkins@getjive.com>
2014-06-17 13:33:44 -0600

    Added number count to peer list

--- html/pbxutils/peer.php
+++ html/pbxutils/peer.php
@@ -6,12 +6,19 @@ $dbconn = pg_connect("host=rodb dbname=pbxs user=postgres ")
     or die('Could not connect: ' . pg_last_error());
 
 // Performing SQL query
-$query = 'SELECT enabled,priority,name,number_format,originates,terminates,supports_fax,supports_emergency FROM peer ORDER BY enabled DESC,priority,name ';
+//$query = 'SELECT enabled,priority,name,number_format,originates,terminates,supports_fax,supports_emergency FROM peer ORDER BY enabled DESC,priority,name ';
+ $query = "SELECT enabled,count(*) as count,priority,name,number_format,originates,terminates,supports_fax,supports_emergency
+  FROM peer
+  LEFT JOIN master_did on (peer.id = master_did.source_peer_id)
+  WHERE enabled = 't' AND master_did.active = 't'
+  GROUP BY peer.id
+  ORDER BY count desc,priority,name";
+
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
 echo "<table border=1>\n";
-echo "<th>enabled</th><th>priority</th><th>name</th><th>number_format</th><th>originates</th><th>terminates</th><th>supports_fax</th><th>supports_emergency</th>\n";
+echo "<th>enabled</th><th>count</th><th>priority</th><th>name</th><th>number_format</th><th>originates</th><th>terminates</th><th>supports_fax</th><th>supports_emergency</th>\n";
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "\t<tr>";
     foreach ($line as $col_value) {
