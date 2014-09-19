2df4be101300a7cf25dc1ab5c57c4a78f35cfbcd
root <root@produtils-1.c1.jiveip.net>
2014-09-19 02:16:28 +0000

    updated time

--- html/pbxutils/events-report.php
+++ html/pbxutils/events-report.php
@@ -154,7 +154,7 @@ if ($action=="search")
 	{	
 		//Actually connect to postgres for the queries we'll be making
     	$eventsDB = pg_connect("host=rodb dbname=events user=postgres ") or die('Could not connect to "events" database: ' . pg_last_error());
-		$eventQ = "SELECT added AT TIME ZONE 'UTC', id, description FROM event WHERE added BETWEEN (timestamp '".$search."' AT TIME ZONE 'America/Boise') AND ((timestamp '".$search."' + interval '1 day') AT TIME ZONE 'America/Boise') ORDER BY added;";
+		$eventQ = "SELECT added AT TIME ZONE 'UTC-6', id, description FROM event WHERE added BETWEEN (timestamp '".$search."' AT TIME ZONE 'America/Boise') AND ((timestamp '".$search."' + interval '1 day') AT TIME ZONE 'America/Boise') ORDER BY added;";
 		$eventArray = pg_fetch_all(pg_query($eventsDB, $eventQ));
 		pg_close($eventsDB);
 		$count = 0;
