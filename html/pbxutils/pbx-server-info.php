6ad10578f25ba66223018698848ca4c63f7940a4
jive-builder <it+jenkins@getjive.com>
2014-10-06 13:00:37 -0600

    added device to server info

--- html/pbxutils/pbx-server-info.php
+++ html/pbxutils/pbx-server-info.php
@@ -45,8 +45,18 @@ while ($load = pg_fetch_assoc($loads)) {
 }
 pg_close($cdrconn);
 
+//Get device count for each domain on this server.
+$query = "SELECT resource_group_id as id, count(id) as count FROM user_agent WHERE resource_group_id IN (SELECT id FROM resource_group WHERE assigned_server = '199.36.251.38') GROUP BY resource_group_id ORDER BY count DESC;";
+$dCount = pg_query($dbconn, $query) or die ('dCount failed: ' . pg_last_error());
+$dValues = Array();
+$cTotal = 0;
+while ($count = pg_fetch_assoc($dCount)){
+	$dValues[$count['id']] = $count['count'];
+	$dTotal += $count['count'];
+}
 
-$query = "SELECT id,domain,name,state,assigned_server,presence_server,location,v5candidate FROM resource_group WHERE assigned_server='" . $assigned_server . "' " . $query_state . " ORDER BY state,domain LIMIT " . $limit;
+
+$query = "SELECT id,domain,name,state,assigned_server,location,v5candidate FROM resource_group WHERE assigned_server='" . $assigned_server . "' " . $query_state . " ORDER BY state,domain LIMIT " . $limit;
 $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
 
 echo "<br/><h2>$assigned_server in <a href='pbx-availability.php?display=$pbxDC'>$pbxDC</a></h2>\n";
@@ -57,19 +67,20 @@ if ($loadPercent > 85) { $color = 'yellow'; }
 if ($loadPercent > 95) { $color = 'red'; }
 
 echo "Server load = $loadTotal, which is <span class='$color'>$loadPercent%</span>";
+echo "<br>Server Device Count = $dTotal";
 
 echo "<table border=1>\n";
-echo "<tr><th>domain</th><th>name</th><th>state</th><th>assigned_server</th><th>load</th><th>presence_server</th><th>location</th><th>v5 candidate</th></tr>\n";
+echo "<tr><th>domain</th><th>name</th><th>state</th><th>assigned_server</th><th>load</th><th>device count</th><th>data location</th><th>v5 candidate</th></tr>\n";
 
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     $count = $count + 1;
-	if ($line['presence_server'] != '') { $santa = "v5 (<a href='presence-server-info.php?server=" . $line['presence_server'] . "'>" . $line['presence_server'] . "</a>)"; } else { $santa = "v4"; }
 	if ($line['location'] == $pbxDC) { $locationColor = 'green'; } else { $locationColor = 'red'; }
 	if ($line['v5candidate'] == 't') { $v5candidate = "TRUE"; }
 	if ($line['v5candidate'] == 'f') { $v5candidate = "false"; }
 	$loadTotal = $loadValues[$line['id']];
 	$loadPercent = round($loadTotal / 140000, 2);
 	$color = 'sky';
+	$dTotal = $dValues[$line['id']];
 	if ($loadPercent > 1) { $color = 'green'; }
 	if ($loadPercent > 10) { $color = 'yellow'; }
 	if ($loadPercent > 20) { $color = 'red'; }
@@ -81,7 +92,7 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 		. "<td>" . $line['state'] . "</td>"
 		. "<td>" . $line['assigned_server'] . "</td>"
 		. "<td class='$color' align='right'>" . $loadPercent . "%</td>"
-		. "<td><center>" . $santa . "</center></td>"
+		. "<td>" . $dTotal . "</td>"
 		. "<td class='$locationColor' align='center'>" . $line['location'] . "</td>"
 		. "<td><center>" . $v5candidate . "</center></td>";
     echo "</tr>\n";
