22afbc8fb569f1f826417c7850b4107c580ec6fa
jive-builder <it+jenkins@getjive.com>
2014-10-14 10:19:42 -0600

    fixed a thing with cost-check

--- html/pbxutils/cost-check.php
+++ html/pbxutils/cost-check.php
@@ -9,7 +9,7 @@
 include('menu.html');
 header('Content-Type:text/html');
 	echo "<h2>Number Cost Finder</h2>";
-	echo "<p>Enter an international number</p></br>";
+	echo "<p>Enter an E.164 without the '+'</p></br>";
 	echo "<form action='' method='GET'>
 		<input type='text' name='number' />
 		<input type='submit' value='Search' />
@@ -20,9 +20,8 @@ if (isset($_REQUEST['number'])) {
 	if (!preg_match('/^\d*$/', $number)) {
 		die ('Not a valid number');
 	}
-	$number = preg_replace('/^011/', '', $number);
 	echo "<p>Number: $number</p>";
-	$curl = curl_init("http://10.117.252.242:6666/score/a/$number");
+	$curl = curl_init("http://10.125.255.66:6666/score/a/$number");
 	print_r(curl_exec($curl));
 	curl_close();
 }
