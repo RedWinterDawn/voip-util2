7dcaa81e4384ae2be0a15bbe28b9118819110fe1
root <root@produtils-1.c1.jiveip.net>
2014-11-05 18:10:54 +0000

    updated time for daylight savings

--- html/pbxutils/events-report.php
+++ html/pbxutils/events-report.php
@@ -147,7 +147,7 @@ if ($action=="search")
 	{	
 		//Actually connect to postgres for the queries we'll be making
     	$eventsDB = pg_connect("host=rodb dbname=events user=postgres ") or die('Could not connect to "events" database: ' . pg_last_error());
-		$eventQ = "SELECT added AT TIME ZONE 'UTC-6', id, description FROM event WHERE added BETWEEN (timestamp '".$search."' AT TIME ZONE 'America/Boise') AND ((timestamp '".$search."' + interval '1 day') AT TIME ZONE 'America/Boise') ORDER BY number desc;";
+		$eventQ = "SELECT added AT TIME ZONE 'UTC-7', id, description FROM event WHERE added BETWEEN (timestamp '".$search."' AT TIME ZONE 'America/Boise') AND ((timestamp '".$search."' + interval '1 day') AT TIME ZONE 'America/Boise') ORDER BY number desc;";
 		$eventArray = pg_fetch_all(pg_query($eventsDB, $eventQ));
 		pg_close($eventsDB);
 		$count = 0;
@@ -234,7 +234,7 @@ if ($action=="eventList" && isset($domainName))
 
 	//Connect to events DB and get events
 	$eventsDB = pg_connect("host=rodb dbname=events user=postgres ") or die('Could not connect to "events" database: ' . pg_last_error());
-	$eventQ = "SELECT added AT TIME ZONE 'UTC-6', description from event, event_domain WHERE domain_id ='".$pbxArray[0]['id']."' AND event_id = id ORDER BY number DESC";
+	$eventQ = "SELECT added AT TIME ZONE 'UTC-7', description from event, event_domain WHERE domain_id ='".$pbxArray[0]['id']."' AND event_id = id ORDER BY number DESC";
 	$eventArray = pg_fetch_all(pg_query($eventsDB, $eventQ)) or die ("Event search failed or no results: " . pg_last_error());
 	pg_close($eventsDB);
 	echo "<h2>".sizeof($eventArray)." Events that affected: " .$domainName . "</h2>";
