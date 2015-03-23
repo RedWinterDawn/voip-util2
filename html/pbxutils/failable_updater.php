d3f379003bf647cfb96aad4d6bed501e8e68750f
jive-builder <it+jenkins@getjive.com>
2015-03-23 17:17:48 -0600

    Adding email alerts for site/ALL changes

--- html/pbxutils/failable_updater.php
+++ html/pbxutils/failable_updater.php
@@ -1,5 +1,7 @@
 <?php
 include('guiltyParty.php');
+$mail_subject="";
+$mail_to="noc@jive.com";
 if (isset($_REQUEST['addr']) && isset($_REQUEST['failable'])) {
   if (strtoupper($_REQUEST['failable']) == "F" || strtoupper($_REQUEST['failable']) == "FALSE") {
     $failable = "f";
@@ -11,8 +13,10 @@ if (isset($_REQUEST['addr']) && isset($_REQUEST['failable'])) {
     $query = "UPDATE pbxstatus SET failable = '$failable' WHERE ip = '$addr';";
   } else if (preg_match('/^[\w-]*$/', $addr)) {
     $query = "UPDATE sitestatus SET failable = '$failable' WHERE site_id = '$addr';";
+	$mail_subject="Abandons toggled for $addr -- now: ";
     if ($addr == "all" || $addr == "universal") {
       $query = "UPDATE sitestatus SET universal_failable = '$failable';";
+	  $mail_subject="Abandons toggled for ALL OF V4 -- now: ";
     }
   } else { 
     die("Error 4");
@@ -31,12 +35,25 @@ $evconn = pg_connect("host=rwdb dbname=events user=postgres") or die ("Error 5")
 
 if ($failable == 't') {
   $insertEvent = "INSERT INTO event (description, event_type) VALUES ('$guiltyParty turned on abandons for $addr', 'FAILABILITY')";
+  $curState = "ON";
 } else {
   $insertEvent = "INSERT INTO event (description, event_type) VALUES ('$guiltyParty turned off abandons for $addr', 'FAILABILITY')";
+  $curState = "OFF";
 } 
 
 $result = pg_query($evconn, $insertEvent) or die ("Error 6");
 
 pg_close($evconn);
 
+if ($mail_subject != "") {
+  
+  //If the mail subject isn't empty, it must be for a site or ALL
+  $mail_headers="From: abandon-toggle@jive.com";
+  $request_time=strftime('%Y-%m-%d %H:%M:%S');
+  $mail_body="User: $guiltyParty\nDate: $request_time\nState: $curState";
+  $mail_subject=$mail_subject+$curState;
+
+  mail($mail_to, $mail_subject, $mail_body, $mail_headers);
+}
+  
 ?>
