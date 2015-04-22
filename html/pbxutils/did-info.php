1432d2ec2eac73553be2a4aeec6240a1af6692c7
jive-builder <it+jenkins@getjive.com>
2015-04-22 13:17:56 -0600

    updated did info

---
+++ html/pbxutils/did-info.php
@@ -0,0 +1,48 @@
+<link rel='stylesheet' href='stylesheet.css'>
+<?php
+include('menu.html');
+include('guiltyParty.php');
+$requestTime = strftime('%Y-%m-%d %H:%M:%S');
+
+if (isset($_GET["did"]))
+{
+	$did = pg_escape_string($_GET["did"]);
+} else
+{
+	die("No DID requested");
+}
+
+// $utildb = pg_connect("host=rodb dbname=util user=postgres ") or die('Could not connect to utildb: '.pg_last_error());
+
+$dbconn = pg_connect("host=rodb dbname=pbxs user=postgres ")
+    or die('Could not connect: ' . pg_last_error());
+
+$didQuery = "SELECT number,active,outbound_routable,destination_pbx_id,source_peer_id,caller_id_name,e911_address_id,directory_address_id,phone_book_delivery_address_id
+   	FROM master_did WHERE number LIKE '" . $did . "%' ORDER BY number ASC LIMIT 10;";
+$didResult = pg_query($didQuery) or die('DID query failed: ' . pg_last_error() . '\n' . '<pre>' . $didQuery . '</pre>');
+
+echo "<table border=2>\n";
+echo "<th>number</th><th>outbound_routable</th><th>caller_id_name</th><th>source peer</th>";
+while ($didRow = pg_fetch_array($didResult, null, PGSQL_ASSOC)) {
+	if ($didRow['outbound_routable'] == 't') {
+		$outbound = "<div class='green'>TRUE</div>";
+	} else {
+		$outbound = "<div class='yellow'>FALSE</div>";
+	}
+
+	echo "<tr>"
+		. "<td>" . $didRow['number'] . "</td>"
+		. "<td><center>" . $outbound . "</center></td>"
+		. "<td>" . $didRow['caller_id_name'] . "</td>"
+		. "<td>" . $didRow['peer'] . "</td>"
+		. "</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($typeResult);
+pg_free_result($didResult);
+
+// Closing connection
+pg_close($dbconn);
+?>
