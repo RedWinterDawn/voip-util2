c77385e8ed730cf169a8f4de70dc2d22b8ea2d4e
jive-builder <it+jenkins@getjive.com>
2015-05-21 20:01:59 -0600

    Updates to tests

--- html/pbxutils/lcr-test.php
+++ html/pbxutils/lcr-test.php
@@ -17,20 +17,31 @@ if (isset($_REQUEST['site'])) {
 }
 
 function testLcrLookup($url){
-	echo "URL: $url";
 	echo "<br/><pre>";
+	echo "URL: $url \n";
 	$curl = curl_init($url);
 	curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,1);
 	curl_setopt($curl,CURLOPT_TIMEOUT,2);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER ,1);
+	curl_setopt($curl, CURLOPT_FRESH_CONNECT ,1);
+	curl_setopt($curl, CURLOPT_FORBID_REUSE ,1);
 	$result = curl_exec($curl);
+	$contentType = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
 	$curl_errno = curl_errno($curl);
 	$curl_error = curl_error($curl);
 	curl_close();
 	if ($curl_errno != 0) { echo "<font color='red'>cURL Error ($curl_errno): $curl_error</font>\n"; }
 	if ($result == '{"results":[]}') { echo "<font color='red'>FAIL: "; } else { echo "<font color='white'>"; }
-	echo "Result: [" . print_r($result) . "]";
-	echo "</font>";
+	#echo "\nResult: [" . print_r($result) . "]";
+	echo "\nResult: [" . $result . "]";
+	echo "</font>\n";
+
+	if ($contentType == 'application/json') {
+	    echo "<font color='green'>Content Type: [" . $contentType . "]</font>\n";
+	}else{
+	    echo "<font color='red'>Content Type: [" . $contentType . "]</font>\n";
+	}
+
 	// echo "<font color='blue'>Blue</font>";
 	echo "</pre>";
 }
@@ -45,19 +56,19 @@ function doAllTheTests($ip){
 	testLcrLookup('http://' . $ip . ':9997/lcr/lookup/e164/180006992000?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
 	testLcrLookup('http://' . $ip . ':9997/lcr/lookup/e164/011320123456?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
 	testLcrLookup('http://' . $ip . ':9997/lcr/lookup/e164/19998887777?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=18019600060&From-Number=');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=18019600060&From-Number=19998887777');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=19998887777&From-Number=');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=011320123456&From-Number=');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=011320123456&From-Number=02');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=18019600060&cli=');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=18019600060&cli=19998887777');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=19998887777&cli=');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=011320123456&cli=');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=011320123456&cli=02');
 	echo "<font color='lightblue'>=== Undefined =============================</font><br/>";
 	testLcrLookup('http://' . $ip . ':9998/lcr/lookup/e164/1801?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
 	testLcrLookup('http://' . $ip . ':9997/lcr/lookup/e164/1801?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=1801&From-Number=');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=1801&cli=');
 	echo "<font color='orange'>=== Fail these =============================</font><br/>";
 	testLcrLookup('http://' . $ip . ':9998/lcr/lookup/e164/0118?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
 	testLcrLookup('http://' . $ip . ':9997/lcr/lookup/e164/0118?contextId=014035c0-01ad-da24-1a10-000100420005&callId=b54eb20-6eb37c1b-6ceef172@10.50.40.58&cli=16785712512');
-	testLcrLookup('http://' . $ip . '/sbc/lcr-validation.php?Dest-Number=0118&From-Number=');
+	testLcrLookup('http://' . $ip . '/sbc/lcr.php?destNumber=0118&cli=');
 }
 
 echo "<h2>LCR Validator tester</h2>";
