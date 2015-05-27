f4ba7a85d0b2389ac208f4126b0516bb42057736
root <root@produtils-2.c1.jiveip.net>
2015-05-27 19:25:07 +0000

    Javascript for Ping Utility

--- html/pbxutils/js/ping.js
+++ html/pbxutils/js/ping.js
@@ -1,13 +1,13 @@
 var counting = 1;
 var doChange = true;
-function pingHost (remoteHost, count) {
+function pingHost (remoteHost, count, dc) {
   if(doChange) {
      $('#ping-status').addClass('status-info').html('Querying...').show();
      $.ajax({
      type: "post",
      url: "ping.php",
      dataType: 'json',
-     data: "remoteHost=" + remoteHost,
+     data: {'remoteHost': remoteHost, 'dc': $('#dc').val()},
      success: function(pingJsonData) {
         $('#ping-row').append($("<div class=\"rowping-packet\">"+counting+"</div>"+pingJsonData.pingInfo).hide().fadeIn(500));
         if(counting >= count) {
@@ -19,14 +19,14 @@ function pingHost (remoteHost, count) {
  }
 }
 
-function ping5 (remoteHost, count) {
+function ping5 (remoteHost, count, dc) {
   counting  = 1;
   doChange = true;
   $('#ping-row').children().remove();
   $('#ping-table').fadeIn();
   for(var i=1; i<=count; i++) {
     nextRun = (i-1) * 1000;
-    setTimeout(pingHost, nextRun, remoteHost, count);
+    setTimeout(pingHost, nextRun, remoteHost, count, dc);
      }
 };
 
@@ -39,7 +39,7 @@ function stopPing () {
 $(document).ready(function() {
       $('#remoteHost').keyup(function(e) {
      if(e.keyCode == 13) {
-     ping5($('#remoteHost').val(), $('#count').val());
+     ping5($('#remoteHost').val(), $('#count').val(), $('#dc').val());
      }
                   });
           $('#ping-table').hide();
