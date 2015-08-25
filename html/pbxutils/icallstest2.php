8ca7bb837ef4a53f6b4d419c8623d5c23749ffce
root <root@produtils-2.c1.jiveip.net>
2015-08-25 16:09:31 +0000

    added distinction per datacenter

--- html/pbxutils/icallstest2.php
+++ html/pbxutils/icallstest2.php
@@ -1,5 +1,24 @@
 <?php
 
+if(isset($_POST['dc'])) {
+        $ip = $_POST['dc'];
+}
+
+if ($ip == '101') {
+  $cmd = "python2.6 pbxcallstats-chi.py";
+}
+elseif ($ip == '125') {
+  $cmd = "python2.6 pbxcallstats-ord.py";
+}
+elseif ($ip == '119') {
+  $cmd = "python2.6 pbxcallstats-lax.py";
+}
+elseif ($ip == '120') {
+  $cmd = "python2.6 pbxcallstats-nyc.py";
+}
+elseif ($ip == '122') {
+  $cmd = "python2.6 pbxcallstats-atl.py";
+}
 $cmd = "python2.6 pbxcallstats.py";
 exec($cmd, $output);
 $test = implode("\n", $output);
